6fdc5494942fe531e65cebe2e4c1a78a2342d59b
CharvyDhand <charvydhand13@gmail.com>
2025-04-07 23:42:24 +0530

    update

--- 02custom react/customreact.js
+++ 02custom react/customreact.js
@@ -1,3 +1,11 @@
+function customRender(reactElement){
+      const domelt=document.createElement(reactElement.type)
+      domelt.innerHTML=reactElement.children
+    //   domelt.setAttribute('href',reactElement.props.href)
+    //   domelt.setAttribute('target',reactElement.props.target)
+    //   root.appendChild(domelt)
+    for()
+    }
 const reactElement= {
     type:'a',
     props:{
@@ -6,8 +14,6 @@ const reactElement= {
     },
     children: 'Click me to visit google'
 }
-
 const root=document.querySelector('#root')
-
 // add reactelement in root
 customRender(reactElement,root)
