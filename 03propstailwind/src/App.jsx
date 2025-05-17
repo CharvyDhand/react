f71e5d7589cee87cb20adc9ef974162ca7c01883
CharvyDhand <charvydhand13@gmail.com>
2025-05-17 22:26:29 +0530

    update

--- 03propstailwind/src/App.jsx
+++ 03propstailwind/src/App.jsx
@@ -5,13 +5,18 @@ import './App.css'
 import Card from './components/Card' 
 function App() {
   const [count, setCount] = useState(0)
-
+  let obj1={
+    username:"charvy"
+  }
+  let arr=[1,2,3]
   return (
     <>
       <h1 className='bg-green-400 rounded-xl'>Tailwind Test</h1>
-      <Card /> 
+      {/* <Card channel="charvy" obj={obj1} newarr={arr}/>  */}
+      <Card username="charvy"/>
+      <Card/>
     </>
   )
 }
 
-export default App
+export default App;
--- 03propstailwind/src/components/Card.jsx
+++ 03propstailwind/src/components/Card.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-function card(){
-    return (
-     <div className="max-w-sm rounded-xl shadow-lg bg-white p-6 mt-4">
-      <img
-        src="https://source.unsplash.com/random/400x200"
-        alt="Card"
-        className="w-full h-48 object-cover rounded-md mb-4"
-      />
-      <h2 className="text-xl font-bold text-gray-800">Card Title</h2>
-      <p className="text-gray-600 mt-2">
-        This is a sample card component using Tailwind CSS in React.
-      </p>
-      <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg">
-        Learn More
-      </button>
+function Card({username}) {                  //function Card(props)
+    console.log("props",username)          // console.log(props.username)
+  return (
+    <div className="p-4 shadow-md rounded-xl">
+      <h2>{username||"hello"}</h2>
     </div>
-    );
+  );
 }
+
+export default Card;
